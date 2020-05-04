93eb11d4e40a4e977a3cc2c61887ae6254b2ded5
MAYANK SUNAL <mayanksunal@gmail.com>
2020-05-04 12:25:34 +0530

    working on forgot password dlogic

--- routes/index.js
+++ routes/index.js
@@ -306,7 +306,7 @@ function(token, user, done) {
    subject: 'project Password Reset',
    text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
      'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-     'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+     'http://' + "photoblog20.herokuapp.com" + '/reset/' + token + '\n\n' +
      'If you did not request this, please ignore this email and your password will remain unchanged.\n'
  };
  smtpTransport.sendMail(mailOptions, function(err) {
